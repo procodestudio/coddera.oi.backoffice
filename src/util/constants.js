b9bcc683c8bf931b0fc54397aeddb4e223ba736d
ramos <rdiego26@gmail.com>
2016-12-13 12:03:09 -0200

    ADD some app configurations file .

---
+++ src/util/constants.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const constants = {
+
+  db: {
+    host: process.env.DB_HOST ||'s2s.coddera.com',
+    username: process.env.DB_USER || 'PROCODE_S2S',
+    password: process.env.DB_PASSWORD || 'PROCODE_S2S',
+    sid: process.env.SID || 'XE',
+    port: process.env.DB_PORT || 15210
+  },
+
+  app: {
+    name: 'OI S2S Backoffice'
+  },
+
+  server: {
+    port: 6000
+  }
+
+};
+module.exports = constants;
