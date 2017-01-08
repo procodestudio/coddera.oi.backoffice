8c6c97add2f503b5da4538c03e3bac157a9d3c5b
ramos <rdiego26@gmail.com>
2017-01-07 22:16:27 -0200

    REMOVE pass ID parameter on save user and checkpint .

--- src/dao/user.js
+++ src/dao/user.js
@@ -55,7 +55,7 @@ let dao = {
         cb(err);
       } else {
         conn.execute(constants.query.user.save,
-          [obj.ID, obj.USUARIO, obj.SENHA, obj.NOME],
+          [obj.USUARIO, obj.SENHA, obj.NOME],
           { autoCommit: true},
           cb);
       }
